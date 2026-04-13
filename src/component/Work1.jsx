233d118dbcd6b99bbb6ced5e3b757d66e9725b65
Amanverma1301 <sewtiverma96@gmail.com>
2026-04-14 02:16:36 +0530

    Refactor Work1 component layout and styles

--- src/component/Work1.jsx
+++ src/component/Work1.jsx
@@ -143,108 +143,56 @@ project61
         className="absolute w-full h-full object-cover opacity-20"
       />   
 
-<Link to='work3'><div
-  onMouseMove={(e) => handleMouseMove(e, e.currentTarget)}
+<Link to='work3'><div 
+ onMouseMove={(e) => handleMouseMove(e, e.currentTarget)}
   onMouseLeave={(e) => handleMouseLeave(e.currentTarget)}
-  className="
-    absolute
-    top-12
-    left-[8vw]
-    w-[90vw]
-    max-w-[550px]
-    aspect-[16/9]
-    transition-transform duration-200
-  "
->
-  <div
-    ref={left2Ref}
-    className="w-full h-full overflow-hidden "
-  />
+className="absolute  top-12 left-[100px] ml-[8vw] w-[550px] h-[300px]  transition-transform duration-200">
+  <div ref={left2Ref} className="w-full h-full  overflow-hidden" />
 
-  {/* TEXT */}
-  <div className="absolute -left-16 md:-left-20 top-2/3 -translate-y-1/2 w-32 md:w-40 z-10">
-    <h2 className="text-lg md:text-2xl font-semibold leading-none">
+ <div className="absolute -left-28 top-[180px] -translate-y-1/2 w-40 z-10">
+    <h2 className="text-[1.5vw] font-semibold leading-none">
       K7 <br /> WEBSITE <br /> UI
     </h2>
-
-    <div className="w-32 md:w-48 mt-3 h-[2px] bg-white"></div>
+    <div className="w-80 ml-auto mt-3 h-[3px] bg-white"></div>
     <p className="mt-3 text-sm opacity-60">03</p>
   </div>
 </div></Link>
 
-{/* CENTER CARD */}
-<Link to="/work5">
-  <div
-    onMouseMove={(e) => handleMouseMove(e, e.currentTarget)}
-    onMouseLeave={(e) => handleMouseLeave(e.currentTarget)}
-    className="
-      absolute
-      right-[5vw]
-      top-[12rem]
-      w-[85vw]
-      max-w-[300px]
-      aspect-[3/5]
-      transition-transform duration-200
-    "
-  >
-    <div
-      ref={center2Ref}
-      className="w-full h-full overflow-hidden "
-    />
 
-    {/* TEXT */}
-    <div className="absolute -left-16 md:-left-20 top-2/3 -translate-y-1/2 z-10">
-      <h2 className="text-lg md:text-2xl font-semibold leading-tight">
-        ECOMMERCE
-      </h2>
-      <h2 className="text-lg md:text-2xl font-semibold leading-tight">
-        WEBSITE
-      </h2>
+<Link to='work5'>
+<div
+ onMouseMove={(e) => handleMouseMove(e, e.currentTarget)}
+  onMouseLeave={(e) => handleMouseLeave(e.currentTarget)}
+className="absolute  right-20 top-50 w-[300px] h-[470px] transition-transform duration-200">
+  <div ref={center2Ref} className="w-full h-full  overflow-hidden" />
 
-      <div className="w-32 md:w-48 h-[2px] bg-white mt-2"></div>
-      <p className="mt-2 text-sm opacity-60">05</p>
-    </div>
+  <div className="absolute -left-20 top-[250px] bottom-6 z-10">
+    <h2 className="text-[1.5vw] font-semibold">ECOMMERCE</h2>
+    <h2 className="text-[1.5vw] font-semibold">WEBSITE</h2>
+    <div className="w-60 h-[3px] bg-white mt-2 ml-auto"></div>
+    <p className="mt-2 text-sm opacity-60">05</p>
   </div>
-
-  {/* Glow Effects */}
-  <div className="absolute top-[23rem] left-[10vw] w-[40vw] max-w-[550px] h-[300px] bg-blue-500/20 blur-[120px] rounded-full"></div>
-  <div className="absolute bottom-20 right-10 w-48 md:w-72 h-48 md:h-72 bg-purple-500/20 blur-[120px] rounded-full"></div>
+</div>
+<div className="absolute top-92 left-[10vw] ml-[8vw] w-[550px] h-[300px] bg-blue-500/20 blur-[120px] rounded-full"></div>
+<div className="absolute bottom-20 right-10 w-72 h-72 bg-purple-500/20 blur-[120px] rounded-full"></div>
 </Link>
+<Link to="work4">
+<div 
+ onMouseMove={(e) => handleMouseMove(e, e.currentTarget)}
+  onMouseLeave={(e) => handleMouseLeave(e.currentTarget)}
+className="absolute bottom-40 right-[700px] w-[280px] h-[420px] transition-transform duration-200">
+  <div ref={right2Ref} className="w-full h-full overflow-hidden" />
 
-
-{/* RIGHT CARD */}
-<Link to="/work4">
-  <div
-    onMouseMove={(e) => handleMouseMove(e, e.currentTarget)}
-    onMouseLeave={(e) => handleMouseLeave(e.currentTarget)}
-    className="
-      absolute
-      bottom-20
-      right-[40vw]
-      w-[100vw]
-      max-w-[280px]
-      aspect-[3/4]
-      transition-transform duration-200
-    "
-  >
-    <div
-      ref={right2Ref}
-      className="w-full h-full overflow-hidden "
-    />
-
-    {/* TEXT */}
-    <div className="absolute -left-16 md:-left-20 -bottom-1 z-10">
-      <h2 className="text-lg md:text-2xl font-semibold">
-        AI Project
-      </h2>
-
-      <div className="w-32 md:w-48 h-[2px] bg-white mt-2"></div>
-      <p className="mt-2 text-sm opacity-60">04</p>
-    </div>
+  <div className=" z-10  -left-20 top-[-140px]  relative">
+    <h2 className="text-[1.5vw] font-semibold">AI Project</h2>
+    <div className="w-60 h-[3px] bg-white mt-2 ml-0"></div>
+    <p className="mt-2 text-sm opacity-60">04</p>
+  </div>
   </div>
 </Link>
 </div>
       {/* CENTER DOT */}
+      <div className="absolute top-1/2 left-[55%] w-3 h-3 border border-white rounded-full opacity-60"></div>
 
     </div>
   );
